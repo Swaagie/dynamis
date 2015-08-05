5f74e47773964549fe69db613b6eaf8f26494b9f
Arnout Kazemier <info@3rd-Eden.com>
2015-08-05 09:55:10 +0200

    [fix] Added missing memcached layer to the support list

--- support.js
+++ support.js
@@ -3,12 +3,13 @@
 //
 // Supported persistence layers.
 //
-exports.enabled = ['cradle', 'redis', 'memory'];
+exports.enabled = ['cradle', 'redis', 'memory', 'memcached'];
 
 //
 // List of stores per persistence layer.
 //
 exports.list = {
+  memcached: 'memcached',
   cradle: 'couchdb',
   memory: 'memory',
   redis: 'redis'
